c9469bac114307516a90400448afd80eefa68155
naneps <ekaprasetya2244@gmail.com>
2022-06-04 12:23:22 +0700

    create register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:auth_ui_flutter/page/constant.dart';
+import 'package:auth_ui_flutter/page/hak_akses_page.dart';
 import 'package:auth_ui_flutter/page/login_page.dart';
+import 'package:auth_ui_flutter/page/resgister_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,9 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.amber),
-      home: LoginPage(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.amber),
+        home: RegisterPage());
   }
 }
